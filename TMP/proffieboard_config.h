d9cfb496e382d3e4cb26389177c99ed4a9646ac3
Mike Honcho <darth.maledous@gmail.com>
2020-05-08 12:00:12 -0700

    Repo clean up

--- TMP/proffieboard_config.h
+++
@@ -1,8 +0,0 @@
-#if PROFFIEBOARD_VERSION - 0 == 1
-#include "proffieboard_v1_config.h"
-#elif PROFFIEBOARD_VERSION - 0 == 2
-#include "proffieboard_v2_config.h"
-#else
-#error UNKNOWN PROFFIEBOARD
-#endif
-
