aa17211d013ad5d740257f756be24c0b6a3ea7ab
Mike Honcho <darth.maledous@gmail.com>
2020-05-15 21:52:06 -0700

    Initial config commit for v4.4 branch

---
+++ kr_luke_v2.h
@@ -0,0 +1,82 @@
+#ifdef CONFIG_TOP
+#include "proffieboard_config.h"
+#define NUM_BLADES 2
+#define NUM_BUTTONS 2
+#define VOLUME 2200
+const unsigned int maxLedsPerStrip = 144;
+#define CLASH_THRESHOLD_G 2.5
+#define ENABLE_AUDIO
+#define ENABLE_MOTION
+#define ENABLE_WS2811
+#define ENABLE_SD
+#define SAVED_PRESET
+#define NO_COLOR_SWING
+#endif
+
+#ifdef CONFIG_PRESETS
+Preset presets[] = {
+{"The_Return", "tracks/Final_Duel.wav",
+//CRYSTAL CHAMBER
+StylePtr<InOutHelper<OnSpark<Green,AudioFlicker<White,GreenYellow>,650>,200,500,Pulsing<Black,Green,4200>>>(),
+
+
+//MAIN BLADE
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<RandomFlicker<Green,GreenYellow>,Pulsing<Gradient<RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>,RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>,RandomFlicker<Green,GreenYellow>>,Pulsing<Gradient<RandomFlicker<Green,GreenYellow>,RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>>,Gradient<RandomFlicker<Green,GreenYellow>,RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>,RandomFlicker<Green,GreenYellow>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Green,GreenYellow>>,1500>,2000>,RandomPerLEDFlicker<Red,White>>,White,80,70>,Red>,GhostWhite,650>,200,500>>(), "The Return"},
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+{"BEspin_CFX_SS", "tracks/sw_jedi_theme1.wav",
+//CRYSTAL CHAMBER
+StylePtr<InOutHelper<OnSpark<Green,AudioFlicker<White,GreenYellow>,650>,200,500,Pulsing<Black,Green,4200>>>(),
+
+
+//MAIN BLADE
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<AudioFlicker<Green,Rgb<5,185,0>>, Pulsing<Gradient<AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>, AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>, AudioFlicker<Green,Rgb<5,185,0>>>, Pulsing<Gradient<AudioFlicker<Green,Rgb<5,185,0>>, AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>>,Gradient<AudioFlicker<Green,Rgb<5,185,0>>, AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>, AudioFlicker<Green,Rgb<5,185,0>>, Strobe<White, BrownNoiseFlicker<White,Red,150>,50,1>,AudioFlicker<Green,Rgb<5,185,0>>>,1500>,2000>, RandomPerLEDFlicker<Red,White>>, White, 90, 80>, GhostWhite, 600, 100, 400>, White, 400>, 200, 500, Black>>(), "BEspin_CFX_SS"},
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+{"Hero_CFX_SS", "tracks/TLJ_Spark.wav",
+//CRYSTAL CHAMBER
+StylePtr<InOutHelper<OnSpark<Green,AudioFlicker<White,GreenYellow>,650>,200,500,Pulsing<Black,Green,4200>>>(),
+
+
+//MAIN BLADE
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Pulsing<Gradient<AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>>,Pulsing<Gradient<AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>>,Gradient<AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,AudioFlicker<Gradient<GreenYellow,Chartreuse>,Gradient<Chartreuse,Green>>>,1500>,2000>,RandomPerLEDFlicker<Red,White>>,White,90,70>,ColorSequence<1000,Red,Rgb<155,0,255>,GhostWhite,Orange>,400>,White,650>,200,500>>(), "Hero_CFX_SS"},
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+{"Mantle_of_the_Force", "tracks/JFO13.wav",
+//CRYSTAL CHAMBER
+StylePtr<InOutHelper<OnSpark<Green,AudioFlicker<White,GreenYellow>,650>,200,500,Pulsing<Black,Green,4200>>>(),
+
+
+//MAIN BLADE
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Pulsing<Gradient<Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>>,Pulsing<Gradient<Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>>,Gradient<Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Blue>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Gradient<Pulsing<GhostWhite,Rgb<5,185,0>,4200>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>,AudioFlicker<Green,Rgb<5,185,0>>>>,1500>,2000>,RandomPerLEDFlicker<Red,White>>,White,90,80>,ColorSequence<1000,Red,Rgb<155,0,255>,GhostWhite>,400>,White,400>,200,500>>(), "Mantle of the Force"},
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+{"Ksith_LS6_SS", "tracks/rotj.wav",
+//CRYSTAL CHAMBER
+StylePtr<InOutHelper<OnSpark<Green,AudioFlicker<White,GreenYellow>,650>,200,500,Pulsing<Black,Green,4200>>>(),
+
+
+//MAIN BLADE
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<Green,Pulsing<Gradient<Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green,Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green,Green>,Pulsing<Gradient<Green,Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green>,Gradient<Green,Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green,Green,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Green>,1500>,2000>,RandomPerLEDFlicker<Red,White>>,White,90,80>,ColorSequence<1000,Red,Rgb<155,0,255>,GhostWhite>,400,75,4000000>,Aquamarine,400>,200,500>>(), "Ksith LS6 SS"},
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+ 
+{ "Calibrate", "tracks/battery_charge1.wav",
+ &style_charging,
+  StyleNormalPtr<BLACK, BLACK, 300, 800>(), "Battery\nLevel"}
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+BladeConfig blades[] = {
+ { 0, 
+    SubBlade(0, 0, WS2811BladePtr<125, WS2811_ACTUALLY_800kHz | WS2811_GRB>()),
+	SubBlade(1, 124, NULL),
+    CONFIGARRAY(presets) },
+};
+#endif
+
+#ifdef CONFIG_BUTTONS
+Button PowerButton(BUTTON_POWER, powerButtonPin, "pow");
+Button AuxButton(BUTTON_AUX, auxPin, "aux");
+#endif
---
+++ sith_acolyte.h
@@ -0,0 +1,56 @@
+#ifdef CONFIG_TOP
+#include "proffieboard_config.h"
+#define NUM_BLADES 1
+#define NUM_BUTTONS 2
+#define VOLUME 2200
+const unsigned int maxLedsPerStrip = 144;
+#define CLASH_THRESHOLD_G 2.5
+#define ENABLE_AUDIO
+#define ENABLE_MOTION
+#define ENABLE_WS2811
+#define ENABLE_SD
+#define NO_COLOR_SWING
+#define SAVED_PRESET
+#endif
+
+#ifdef CONFIG_PRESETS
+Preset presets[] = {
+{ "Violence", "tracks/swtor_epic2.wav",
+StylePtr<InOutHelper<OnSpark<SimpleClash<Lockup<Blast<HumpFlicker<Orange,Red,50>,Cylon<GhostWhite,50,100,RandomPerLEDFlicker<Magenta,Cyan>,40,25,100>,1200,50>,Gradient<StyleFire<HumpFlicker<Green,Magenta,50>,RandomPerLEDFlicker<Black,Magenta>,0,2,FireConfig<0,2000,1>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,5>>,BrownNoiseFlicker<Pulsing<GhostWhite,Red,2800>,Pulsing<GhostWhite,Yellow,2800>,50>,BrownNoiseFlicker<Black,RandomPerLEDFlicker<Tomato,Magenta>,50>,StyleFire<HumpFlicker<Green,Magenta,50>,BrownNoiseFlicker<Black,Magenta,50>,0,2,FireConfig<0,2000,2>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,1>>>,AudioFlicker<Blue,White>>,Gradient<StyleFire<DarkOrange,Black>,BrownNoiseFlicker<White,Magenta,50>,StyleFire<Red,Red,0,2,FireConfig<0,2000,5>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,2>>>,240>,White,800>,190,500>>(), "red_orange"},
+ 
+{"KSith_Ascension", "tracks/swtor_epic3.wav",
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Pulsing<Gradient<Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Strobe<Yellow,BrownNoiseFlicker<Red,White,100>,50,1>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>>,Gradient<Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Strobe<Yellow,BrownNoiseFlicker<Red,White,100>,50,1>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>,Pulsing<RandomFlicker<Red,Rgb<255,65,5>>,AudioFlicker<Red,Black>,1000>>,3500>,RandomPerLEDFlicker<Red,White>>,AudioFlicker<Yellow,LightYellow>,90,95>,White>,White,600>,200,500>>(), "maul"},
+
+
+{ "cyber", "tracks/swtor_darksidetheme5.wav",
+StylePtr<InOutHelper<OnSpark<SimpleClash<Lockup<Blast<RandomFlicker<Rgb<200,5,255>,Black>,Cylon<GhostWhite,50,100,Blinking<RandomPerLEDFlicker<GreenYellow,Red>,BrownNoiseFlicker<Pink,OrangeRed,50>,100,500>,25,37,100>,1200,50>,Gradient<StyleFire<HumpFlicker<Green,Magenta,50>,RandomPerLEDFlicker<Black,Magenta>,0,2,FireConfig<0,2000,1>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,5>>,BrownNoiseFlicker<Pulsing<GhostWhite,Red,2800>,Pulsing<GhostWhite,Yellow,2800>,50>,BrownNoiseFlicker<Black,RandomPerLEDFlicker<Tomato,Magenta>,50>,StyleFire<HumpFlicker<Green,Magenta,50>,BrownNoiseFlicker<Black,Magenta,50>,0,2,FireConfig<0,2000,2>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,1>>>,AudioFlicker<Blue,White>>,Gradient<StyleFire<DarkOrange,Black>,BrownNoiseFlicker<White,Magenta,50>,StyleFire<Red,Red,0,2,FireConfig<0,2000,5>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,2>>>,240>,BrownNoiseFlicker<GhostWhite,Black,75>,1200>,190,500>>(), "purple"},
+   
+
+{ "Menace", "tracks/MSV.wav",
+StylePtr<InOutHelper<OnSpark<SimpleClash<Lockup<Blast<AudioFlicker<Red,Rgb<155,0,0>>,ColorSequence<500,Cyan,Green,Yellow>,400>,Gradient<StyleFire<HumpFlicker<Green,Magenta,50>,RandomPerLEDFlicker<Black,Magenta>,0,2,FireConfig<0,2000,1>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,5>>,BrownNoiseFlicker<Pulsing<GhostWhite,Red,2800>,Pulsing<GhostWhite,Yellow,2800>,50>,BrownNoiseFlicker<Black,RandomPerLEDFlicker<Tomato,Magenta>,50>,StyleFire<HumpFlicker<Green,Magenta,50>,BrownNoiseFlicker<Black,Magenta,50>,0,2,FireConfig<0,2000,2>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,1>>>,AudioFlicker<Blue,White>>,Gradient<StyleFire<DarkOrange,Black>,BrownNoiseFlicker<White,Magenta,50>,StyleFire<Red,Red,0,2,FireConfig<0,2000,5>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,2>>>,240>,White,800>,190,500>>(), "red"},
+	
+   
+{"Pitch-Black", "tracks/swtor_darksidetheme4.wav",
+StylePtr<InOutHelper<OnSpark<Blast<LocalizedClash<Lockup<RandomFlicker<Red,Rgb<180,0,0>>,Pulsing<Gradient<RandomFlicker<Red,Rgb<180,0,0>>,RandomFlicker<Red,Rgb<180,0,0>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Red,Rgb<180,0,0>>,RandomFlicker<Red,Rgb<180,0,0>>>,Gradient<RandomFlicker<Red,Rgb<180,0,0>>,RandomFlicker<Red,Rgb<180,0,0>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Red,Rgb<180,0,0>>,RandomFlicker<Red,Rgb<180,0,0>>,RandomFlicker<Red,Rgb<180,0,0>>>,3500>,RandomPerLEDFlicker<Red,White>>,ColorSequence<500,GhostWhite,Yellow>,90,70>,ColorSequence<650,Green,Rgb<155,0,255>,GhostWhite,Yellow>,400>,GhostWhite,575>,200,500>>(), "red"},
+
+
+{"Unlightstable", "tracks/swtor_darksidetheme2.wav",
+StylePtr<InOutHelper<OnSpark<SimpleClash<Lockup<Blast<HumpFlicker<Orange,Red,50>,Cylon<GhostWhite,50,100,RandomPerLEDFlicker<Magenta,Cyan>,40,25,100>,1200,50>,Gradient<StyleFire<HumpFlicker<Green,Magenta,50>,RandomPerLEDFlicker<Black,Magenta>,0,2,FireConfig<0,2000,1>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,5>>,BrownNoiseFlicker<Pulsing<GhostWhite,Red,2800>,Pulsing<GhostWhite,Yellow,2800>,50>,BrownNoiseFlicker<Black,RandomPerLEDFlicker<Tomato,Magenta>,50>,StyleFire<HumpFlicker<Green,Magenta,50>,BrownNoiseFlicker<Black,Magenta,50>,0,2,FireConfig<0,2000,2>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,1>>>,AudioFlicker<Blue,White>>,Gradient<StyleFire<DarkOrange,Black>,BrownNoiseFlicker<White,Magenta,50>,StyleFire<Red,Red,0,2,FireConfig<0,2000,5>,FireConfig<3000,0,0>,FireConfig<0,5000,10>,FireConfig<0,0,2>>>,240>,White,800>,190,500>>(), "red_orange"},
+
+
+
+{"The_Second", "tracks/swtor_epic1.wav",
+StylePtr<InOutHelper<OnSpark<OriginalBlast<LocalizedClash<Lockup<RandomFlicker<Rgb<155,65,10>,Rgb<180,100,0>>,Pulsing<Gradient<Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>>,Pulsing<Gradient<Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>>,Gradient<Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Gradient<Pulsing<BrownNoiseFlicker<Yellow,Black,200>,BrownNoiseFlicker<Red,Black,200>,3000>,RandomFlicker<DarkOrange,Yellow>,RandomFlicker<DarkOrange,Yellow>>>,1500>,2000>,RandomPerLEDFlicker<Red,White>>,White,90,75>,RandomPerLEDFlicker<Cyan,LavenderBlush>>,White,400>,200,500>>(),"second"},
+ 
+   { "Calibrate", "tracks/batteryhum.wav",
+    &style_charging, "Battery\nLevel"}
+};
+BladeConfig blades[] = {
+ { 0, WS2811BladePtr<118, WS2811_ACTUALLY_800kHz | WS2811_GRB>(), CONFIGARRAY(presets) },
+};
+#endif
+
+#ifdef CONFIG_BUTTONS
+Button PowerButton(BUTTON_POWER, powerButtonPin, "pow");
+Button AuxButton(BUTTON_AUX, auxPin, "aux");
+#endif
