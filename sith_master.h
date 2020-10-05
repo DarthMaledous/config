f1d7faa3f3f57ae9f9900be500f4f15676521a7f
Mike Honcho <darth.maledous@gmail.com>
2020-10-04 21:47:18 -0700

    Increase clash threshold and lower volume

--- sith_master.h
+++ sith_master.h
@@ -2,9 +2,9 @@
 #include "proffieboard_config.h"
 #define NUM_BLADES 1
 #define NUM_BUTTONS 1
-#define VOLUME 2000
+#define VOLUME 1800
 const unsigned int maxLedsPerStrip = 144;
-#define CLASH_THRESHOLD_G 2.5
+#define CLASH_THRESHOLD_G 3.0
 #define ENABLE_AUDIO
 #define ENABLE_MOTION
 #define ENABLE_WS2811
