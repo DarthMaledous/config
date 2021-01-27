803fae6c7e2d560cefcc9a76ae4b4c66760963d9
Mike Honcho <darth.maledous@gmail.com>
2021-01-27 00:25:00 -0800

    Config updates

--- 89-Sabers_Fallen_V2.h
+++ 89-Sabers_Fallen_V2.h
@@ -9,56 +9,51 @@ const unsigned int maxLedsPerStrip = 144;
 #define ENABLE_MOTION
 #define ENABLE_WS2811
 #define ENABLE_SD
+#define NO_COLOR_SWING
+#define IDLE_OFF_TIME 90 * 1000
+#define SAVE_STATE
+#define SA22C_TWIST_ON
+#define SA22C_TWIST_OFF
+#endif
+
+#ifdef CONFIG_PROP
+#include "../props/saber_sa22c_buttons.h"
 #endif
 
 #ifdef CONFIG_PRESETS
 Preset presets[] = {
-   { "TeensySF", "tracks/venus.wav",
-    StyleNormalPtr<CYAN, WHITE, 300, 800>(),
-    StyleNormalPtr<CYAN, WHITE, 300, 800>(), "cyan"},
-   { "SmthJedi", "tracks/mars.wav",
-    StylePtr<InOutSparkTip<EASYBLADE(BLUE, WHITE), 300, 800> >(),
-    StylePtr<InOutSparkTip<EASYBLADE(BLUE, WHITE), 300, 800> >(), "blue"},
-   { "SmthGrey", "tracks/mercury.wav",
-    StyleFirePtr<RED, YELLOW, 0>(),
-    StyleFirePtr<RED, YELLOW, 1>(), "fire"},
-   { "SmthFuzz", "tracks/uranus.wav",
+{ "FallenOrder", "tracks/venus.wav",	StylePtr<Layers<Stripes<16000,-1000,RotateColorsX<Variation,Blue>,Pulsing<RotateColorsX<Variation,Rgb<0,0,90>>,RotateColorsX<Variation,Blue>,800>,RotateColorsX<Variation,Blue>>,TransitionEffectL<TrConcat<TrFade<600>,RandomFlicker<RotateColorsX<Variation,Blue>,RotateColorsX<Variation,Rgb<0,0,160>>>,TrDelay<30000>,RotateColorsX<Variation,Blue>,TrFade<800>>,EFFECT_FORCE>,TransitionEffectL<TrConcat<TrFade<400>,AudioFlickerL<RotateColorsX<Variation,Rgb<150,150,255>>>,TrDelay<30000>,AudioFlickerL<RotateColorsX<Variation,Rgb<150,150,255>>>,TrFade<800>>,EFFECT_FORCE>,LockupTrL<Layers<AlphaL<AudioFlickerL<Azure>,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Scale<SwingSpeed<100>,Int<14000>,Int<18000>>>>,AlphaL<White,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Int<10000>>>>,TrConcat<TrInstant,AlphaL<TransitionEffect<Azure,White,TrInstant,TrFade<200>,EFFECT_LOCKUP_BEGIN>,Bump<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Int<20000>>>,TrFade<400>>,TrConcat<TrInstant,AlphaL<Mix<SmoothStep<Scale<BladeAngle<>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>>,Int<1000>>,Stripes<1500,2000,TransitionEffect<White,Azure,TrInstant,TrFade<200>,EFFECT_LOCKUP_BEGIN>,Blue>,Stripes<1500,-2500,TransitionEffect<White,Azure,TrInstant,TrFade<200>,EFFECT_LOCKUP_BEGIN>,Blue>>,Int<18000>>,TrFade<400>>,SaberBase::LOCKUP_NORMAL>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,ResponsiveStabL<Red,TrWipeIn<600>,TrWipe<600>>,ResponsiveBlastWaveL<TransitionEffect<Azure,White,TrInstant,TrFade<100>,EFFECT_BLAST>,Int<400>,Scale<SwingSpeed<200>,Int<100>,Int<400>>,Int<400>>,ResponsiveClashL<White,TrInstant,TrFade<400>,Scale<BladeAngle<0,16000>,Int<4000>,Int<26000>>,Int<6000>,Int<20000>>,TransitionEffectL<TrConcat<TrInstant,HumpFlickerL<White,40>,TrFade<800>>,EFFECT_IGNITION>,LockupTrL<AlphaL<BrownNoiseFlickerL<White,Int<300>>,SmoothStep<Int<30000>,Int<5000>>>,TrWipeIn<400>,TrFade<300>,SaberBase::LOCKUP_DRAG>,LockupTrL<AlphaL<Mix<TwistAngle<>,Red,Orange>,SmoothStep<Int<28000>,Int<5000>>>,TrWipeIn<600>,TrFade<300>,SaberBase::LOCKUP_MELT>,InOutTrL<TrJoinR<TrWipe<300>,TrWipeIn<300>>,TrWipeInX<Scale<IsLessThan<BladeAngle<>,Int<16000>>,Int<300>,Int<500>>>,Black>>>(),
+    
+StylePtr<Layers<Blue,RandomL<Rgb<0,10,100>>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<RotateColorsX<Variation,Rgb<50,130,255>>,Int<100>>,TrFade<1200>>,EFFECT_IGNITION>,InOutTrL<TrFade<500>,TrInstant,Pulsing<Black,Blue,3000>>>>(), "Fallen Order"},
+/////////////////////////////////////////////////////////////////////////////////
+
+{ "Survivor_Blue", "tracks/mars.wav",
+StylePtr<Layers<Rgb<0,40,195>,AudioFlickerL<Blue>,ResponsiveLightningBlockL<White>,ResponsiveLockupL<BrownNoiseFlickerL<NavajoWhite,Int<25>>,TrInstant,TrInstant,Int<26000>>,ResponsiveMeltL<Mix<TwistAngle<>,Red,Yellow>>,ResponsiveDragL<White>,ResponsiveClashL<White,TrInstant,TrFade<200>,Int<26000>>,ResponsiveBlastL<White>,ResponsiveBlastWaveL<White>,ResponsiveBlastFadeL<White>,ResponsiveStabL<White>,InOutTrL<TrWipe<250>,TrWipeIn<500>>>>(),
+	    StylePtr<Layers<Rgb<0,10,100>,RandomL<Blue>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<RotateColorsX<Variation,Rgb<50,130,255>>,Int<100>>,TrFade<1200>>,EFFECT_IGNITION>,InOutTrL<TrFade<500>,TrInstant,Pulsing<Black,SequenceL<Blue,100,37,0b1010100011100,0b111000111000101,0b100000000000000>,3000>>>>(), "Survivor Blue"},
+////////////////////////////////////////////////////////////////////////////////   
+   
+	{ "Photon_Blade", "tracks/uranus.wav",
     StyleNormalPtr<RED, WHITE, 300, 800>(),
     StyleNormalPtr<RED, WHITE, 300, 800>(), "red"},
-   { "RgueCmdr", "tracks/venus.wav",
+  
+	{ "Outcast_Knight", "tracks/venus.wav",
     StyleFirePtr<BLUE, CYAN, 0>(),
     StyleFirePtr<BLUE, CYAN, 1>(), "blue fire"},
-   { "TthCrstl", "tracks/mars.wav",
+   
+	{ "Ancient", "tracks/mars.wav",
     StylePtr<InOutHelper<EASYBLADE(OnSpark<GREEN>, WHITE), 300, 800> >(),
     StylePtr<InOutHelper<EASYBLADE(OnSpark<GREEN>, WHITE), 300, 800> >(), "green"},
-   { "TeensySF", "tracks/mercury.wav",
-    StyleNormalPtr<WHITE, RED, 300, 800, RED>(),
-    StyleNormalPtr<WHITE, RED, 300, 800, RED>(), "white"},
-   { "SmthJedi", "tracks/uranus.wav",
-    StyleNormalPtr<AudioFlicker<YELLOW, WHITE>, BLUE, 300, 800>(),
-    StyleNormalPtr<AudioFlicker<YELLOW, WHITE>, BLUE, 300, 800>(), "yellow"},
-   { "SmthGrey", "tracks/venus.wav",
-    StylePtr<InOutSparkTip<EASYBLADE(MAGENTA, WHITE), 300, 800> >(),
-    StylePtr<InOutSparkTip<EASYBLADE(MAGENTA, WHITE), 300, 800> >(), "magenta"},
-   { "SmthFuzz", "tracks/mars.wav",
-    StyleNormalPtr<Gradient<RED, BLUE>, Gradient<CYAN, YELLOW>, 300, 800>(),
-    StyleNormalPtr<Gradient<RED, BLUE>, Gradient<CYAN, YELLOW>, 300, 800>(), "gradient"},
-   { "RgueCmdr", "tracks/mercury.wav",
-    StyleRainbowPtr<300, 800>(),
-    StyleRainbowPtr<300, 800>(), "rainbow"},
-   { "TthCrstl", "tracks/uranus.wav",
-    StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(),
-    StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(), "strobe"},
-   { "TeensySF", "tracks/venus.wav",
-    &style_pov,
-    StyleNormalPtr<BLACK, BLACK, 300, 800>(), "POV"},
-   { "SmthJedi", "tracks/mars.wav",
+  
+	{ "Peace", "tracks/",
+	 
+	
+	{ "SmthJedi", "tracks/mars.wav",
     &style_charging,
     StyleNormalPtr<BLACK, BLACK, 300, 800>(), "Battery\nLevel"}
 };
 BladeConfig blades[] = {
  { 0, WS281XBladePtr<144, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(),
-      SubBlade(0, 0,  WS281XBladePtr<1, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin4> >()
+    WS281XBladePtr<1, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin4> >()
   , CONFIGARRAY(presets) },
 };
 #endif
